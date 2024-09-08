4a68f16d5c7b3deb9ef59bc1c1a712ceda1d1958
Msingh12345 <msinghsvm@gmail.com>
2024-09-08 23:04:48 +0530

    makes some changes

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 import express from "express";
+import bodyParser from "body-parser";
 
-const app=express();
-const port=3333;
+ 
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const dr = dirname(fileURLToPath(import.meta.url));
+ 
+const app = express();
+const port = 3333;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 
+app.get("/", (req, res) => {
+  res.sendFile(dr + "/public/index.html");
+});
 
-app.get("", (req, res) => {
-    res.send("hjj");
-  });
+app.get("/", (req, res) => {
+  res.render("index.html");
+});
+
+app.post("/submit", (req, res) => {
+  console.log(req.body)
+});
+
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-  });
+  console.log(`Listening on port ${port}`);
+});
+
+
+
+
+
+
+// //   echo "# To-do-list" >> README.md
+// // git init
+// // git add README.md
+// // git commit -m "first commit"
+// // git branch -M main
+// // git remote add origin https://github.com/Msingh12345/To-do-list.git
+// // git push -u origin main
+
+
+
+
