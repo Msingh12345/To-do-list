4e539bc1c110b9bb3c2d52964979195c28bc0b19
Msingh12345 <msinghsvm@gmail.com>
2024-09-08 21:23:09 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app=express();
+const port=3333;
+
+
+
+app.get("", (req, res) => {
+    res.send("hjj");
+  });
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
