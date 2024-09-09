9d75fb308d630f807210e00ba544974084c34aac
Msingh12345 <msinghsvm@gmail.com>
2024-09-09 15:28:02 +0530

    to-do-app complete

--- index.js
+++ index.js
@@ -1,28 +1,89 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
- 
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const dr = dirname(fileURLToPath(import.meta.url));
- 
+
 const app = express();
 const port = 3333;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "todo",
+  password: "msingh12345@",
+  port: 5432,
+});
+db.connect();
 
-app.get("/", (req, res) => {
-  res.sendFile(dr + "/public/index.html");
+let items = [{ id: 1, title: "buymilk" }];
+console.log(items);
+
+// db.query("SELECT * FROM todotable ORDER BY id ASC", (err, res) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     items = res.rows;
+//     console.log(items);
+//   }
+// });
+
+
+
+app.get("/", async(req, res) => {
+  try {
+    const result = await db.query("SELECT * FROM todotable ORDER BY id ASC");
+    items = result.rows;
+    console.log(items);
+    res.render("index.ejs", {
+      listitem: "Today",
+      item: items,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-app.get("/", (req, res) => {
-  res.render("index.html");
+
+app.post("/add", async(req, res) => {
+  const it = req.body.newItem;
+  // items.push({title: item});
+  try {
+    await db.query("INSERT INTO todotable (title) VALUES ($1)", [it]);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-app.post("/submit", (req, res) => {
-  console.log(req.body)
+
+app.post("/delete", async (req, res) => {
+  const id = req.body.deleteItemId;
+  try {
+    await db.query("DELETE FROM todotable WHERE id = $1", [id]);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+
+
+app.post("/edit", async (req, res) => {
+  const it = req.body.updatedItemTitle;
+  const id = req.body.updatedItemId;
+
+  try {
+    await db.query("UPDATE todotable SET title = ($1) WHERE id = $2", [it, id]);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.listen(port, () => {
@@ -34,14 +95,11 @@ app.listen(port, () => {
 
 
 
-// //   echo "# To-do-list" >> README.md
-// // git init
-// // git add README.md
-// // git commit -m "first commit"
-// // git branch -M main
-// // git remote add origin https://github.com/Msingh12345/To-do-list.git
-// // git push -u origin main
 
 
 
+
+// app.get("/", (req, res) => {
+//   res.sendFile(dr + "/public/index.html");
+// });
 
